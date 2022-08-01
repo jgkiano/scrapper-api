0e52cc68d64f3aa4df58982ced21947a713adda5
Julius Kiano <jgkiano@gmail.com>
2022-08-01 15:23:34 +0300

    created customer schema

---
+++ src/schemas/customer.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Customer {
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true })
+  phoneNumber: string;
+
+  @Prop({ required: true })
+  bvn: string;
+}
+
+export type CustomerDocument = Customer & Document;
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
