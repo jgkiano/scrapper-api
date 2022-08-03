a47c73ab1705e491af95ec1ba05548136799dd1b
Julius Kiano <jgkiano@gmail.com>
2022-08-03 11:22:27 +0300

    test scrapper service e2e

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,38 +1,37 @@
 import { Controller, Get } from '@nestjs/common';
-import { TransactionService } from '../services/transaction.service';
+import { AuthService } from '../services/auth.service';
+import { CustomerService } from '../services/customer.service';
+import { OrganizationService } from '../services/organization.service';
+import { ScrapperService } from '../services/scrapper.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly service: TransactionService) {}
+  constructor(
+    private readonly scrapperService: ScrapperService,
+    private readonly organizationSerivce: OrganizationService,
+    private readonly customerService: CustomerService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   async getHello() {
-    const data = await this.service.createBulkTransactions([
-      {
-        organizationId: '62e7ea4d7c4a7cc849d8d53c',
-        customerId: '62e7eac3b51306f0d5c8f81f',
-        amount: 301,
-        beneficiaryAccountNumber: '4048883915',
-        currency: '$',
-        date: new Date(),
-        senderAccountNumber: '7154854816',
-        type: 'credit',
-        description:
-          'deposit transaction at Ledner, Mohr and Braun using card ending with ***(...6957) for MVR 606.64 in account ***89731944',
-      },
-      {
-        organizationId: '62e7ea4d7c4a7cc849d8d53c',
-        customerId: '62e7eac3b51306f0d5c8f81f',
-        amount: 302,
-        beneficiaryAccountNumber: '4048883915',
-        currency: '$',
-        date: new Date(),
-        senderAccountNumber: '7154854816',
-        type: 'credit',
-        description:
-          'deposit transaction at Ledner, Mohr and Braun using card ending with ***(...6957) for MVR 606.64 in account ***89731944',
-      },
-    ]);
-    return data.map((item) => item.toJSON());
+    const org = await this.organizationSerivce.createOrganization({
+      loginUrl: 'https://bankof.okra.ng/login',
+      name: 'okra bank',
+    });
+    const user = await this.customerService.createUser({
+      bvn: '123456789',
+      firstName: 'Julius',
+      lastName: 'Kiano',
+      phoneNumber: '+254700110590',
+    });
+    const auth = await this.authService.createAuth({
+      customerId: user.id,
+      organizationId: org.id,
+      password: 'b9fBOwsPExnakcTu',
+      username: 'hi@kiano.me',
+    });
+    const data = this.scrapperService.scrape(org.id, user.id);
+    return data;
   }
 }
