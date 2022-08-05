c2d71d3b7154b296aea961bf07e40687b0a3c7e2
Julius Kiano <jgkiano@gmail.com>
2022-08-05 20:07:40 +0300

    renamed to plural

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -3,7 +3,7 @@ import { OrganizationDocument } from '../schemas/organization.schema';
 import { OrganizationService } from '../services/organization.service';
 import { CreateOrganizationDto } from '../types';
 
-@Controller('/organization')
+@Controller('/organizations')
 export class OrganizationController {
   constructor(private readonly organizationSerivce: OrganizationService) {}
   @Post()
