f5639bf465a60f6ea5a4297fa4cdde919224e6de
Julius Kiano <jgkiano@gmail.com>
2022-08-08 14:13:18 +0300

    use type inference

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { OrganizationDocument } from '../schemas/organization.schema';
 import { OrganizationService } from '../services/organization.service';
 import { CreateOrganizationDto } from '../types';
 
@@ -9,7 +8,7 @@ export class OrganizationController {
   @Post()
   async createOrganization(
     @Body() createOrganizationDto: CreateOrganizationDto,
-  ): Promise<OrganizationDocument> {
+  ) {
     return this.organizationSerivce.createOrganization(createOrganizationDto);
   }
 }
